da56ee6609e1e911d330521f499f7d89473b784b
ygr99 <196156709@qq.com>
2023-12-13 18:48:01 +0800

    Initial commit 复制 鱼皮 - 用户中心项目 主要运用阿里 Ant Design 生态： HTML + CSS + JavaScript 三件套 React 开发框架 Ant Design Pro 项目模板 Ant Design 端组件库 Umi 开发框架 Umi Request 请求库

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://wangguanjingji.oss-cn-beijing.aliyuncs.com/picture/favicon.png";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://docs.qq.com/doc/DUG93dVNHbVZjZXpo";
